1901b4c5913c4308dfc859099a71d7b2b61b9517
24april <167975514+24april@users.noreply.github.com>
2024-05-21 11:03:46 +0300

    Update passwordrandom.py

--- Practice/passwordrandom.py
+++ Practice/passwordrandom.py
@@ -1,9 +1,9 @@
 import random
-def randompassword(lenghth):
+def randompassword(length):
     password=""
     while ((password.islower()==False) and (password.isupper()==False) and any(i.isdigit() for i in password) and (any(i.isalnum()==False for i in password)) and any(i.isalpha() for i in password))==False:
         password=""
-        for _ in range(lenghth):
+        for _ in range(length):
             password+=random.choice(r"""#$%&'()*+,-./:\;<=>?@[]^_`{|}~1234567890QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm"!""")
     return password
 print(randompassword(4))
